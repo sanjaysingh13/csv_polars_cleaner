b39ed889cc10fb52c229c23b46c9053ddb9f209c
Sanjay <sanjaysingh13@gmail.com>
2025-04-30 15:32:49 +0530

    Add static files for Rustdoc styling

---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["csv_polars_cleaner",["",[],["detector.rs","lib.rs","parser.rs","preprocessor.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[85]}
